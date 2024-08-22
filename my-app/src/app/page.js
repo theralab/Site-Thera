90579daae62e7dec5e8771b1e2f874f1ad85c45f
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2024-08-22 00:41:41 -0300

    Implementação inicial da header (nav bar) do site

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import Header from './Header'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen">
+    <main>
+      <Header />
     </main>
   );
 }
