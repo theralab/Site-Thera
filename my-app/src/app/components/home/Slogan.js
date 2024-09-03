0bd4596f1d2f1b1f346237b48110c64e8590627d
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2024-09-02 23:56:19 -0300

    fix: Correção do comportamento do Slogan

--- my-app/src/app/components/home/Slogan.js
+++ my-app/src/app/components/home/Slogan.js
@@ -1,10 +1,8 @@
 export default function Slogan() {
   return (
-    <div className="flex flex-col justify-center items-center text-white py-3 text-pretty">
+    <div className="flex flex-col justify-center items-center text-white py-3 text-pretty text-center mx-8">
       <h3 className="text-lg">
-        {/* TODO: Melhor maneira de fazer isso (sem o br). Não consegui com text-balance*/}
-        Inicie sua carreira conosco, com <br />
-        projetos reais desde o começo
+        Inicie sua carreira conosco, com projetos reais desde o começo
       </h3>
       <span className="font-extralight text-sm">
         Feito por alunos para alunos
