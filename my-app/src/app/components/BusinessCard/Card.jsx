faacaf9fee16753ce4105af1f58b2202803a076b
MonteiroBySWK <eumonteiro.ofc@gmail.com>
2024-09-04 17:36:57 -0300

    modificação para ter borda somente na parte superior

--- my-app/src/app/components/BusinessCard/Card.jsx
+++ my-app/src/app/components/BusinessCard/Card.jsx
@@ -1,15 +1,9 @@
 export default function Card({ title, description }) {
 
-  const styleCard = {
-
-  }
-
   return (
-    <div className="row-span-2 col-span-1 bg-gradient-to-r p-0.5 from-pink-500 to-violet-600  rounded-md ">
+    <div className="row-span-2 col-span-1 pt-0.5 drop-shadow-xl bg-gradient-to-r from-pink-500 to-violet-600 duration-300 rounded-md ">
       <div className="bg-white pb-8 rounded px-4 pt-1.5 h-full">
-
         <p className="font-semibold text-lg mb-1.5">{title}</p>
-
         <p className="font-light lg:w-full w-48">{description}</p>
       </div>
     </div>
