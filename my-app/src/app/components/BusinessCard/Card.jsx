ccda50b78284044f3c4cbff2014e7321c5c96b51
MonteiroBySWK <eumonteiro.ofc@gmail.com>
2024-09-04 17:20:05 -0300

    adição das bordas em gradientes dos cartões

--- my-app/src/app/components/BusinessCard/Card.jsx
+++ my-app/src/app/components/BusinessCard/Card.jsx
@@ -1,9 +1,17 @@
 export default function Card({ title, description }) {
 
+  const styleCard = {
+
+  }
+
   return (
-    <div className="col-span-1 border pb-8 border-purple-600 rounded-md px-4 pt-1.5">
-      <p className="font-semibold text-lg mb-1.5">{title}</p>
-      <p className="font-light lg:w-full w-48">{description}</p>
+    <div className="row-span-2 col-span-1 bg-gradient-to-r p-0.5 from-pink-500 to-violet-600  rounded-md ">
+      <div className="bg-white pb-8 rounded px-4 pt-1.5 h-full">
+
+        <p className="font-semibold text-lg mb-1.5">{title}</p>
+
+        <p className="font-light lg:w-full w-48">{description}</p>
+      </div>
     </div>
   )
 }
