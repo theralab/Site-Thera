1863b2ac9959ba8c498ac7cd53cc0ad585a5e16f
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2024-09-06 10:41:12 -0300

    Pequenas mudanças no código para o grid de ferramentas, e mudanças sutis na estilização do grid de linguagens.

--- my-app/src/app/components/ToolGrid/Tool.jsx
+++ my-app/src/app/components/ToolGrid/Tool.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 
 export default function Tool(props) {
-  const size = 45; // Tamanho da imagem
+  const size = 50; // Tamanho da imagem
 
   return (
     <div className="flex justify-center">
-      <div className="flex items-center justify-center shadow-gray-400/50 shadow-md bg-white p-6 border rounded-xl h-[74px] w-32 overflow-hidden">
+      <div className="flex items-center justify-center shadow-gray-400/50 shadow-md bg-white p-6 border rounded-xl h-[74px] w-32">
         <Image
           src={props.symbol}
           alt="tool"
