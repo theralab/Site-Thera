72cebef6173b37a06ba1f40025ab5b71822eac9c
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2024-09-08 14:53:41 -0300

    Responsividade para ToolSection

--- my-app/src/app/components/home/PresentationSection.js
+++ my-app/src/app/components/home/PresentationSection.js
@@ -5,17 +5,17 @@ export default function PresentationSection() {
     <div>
       <div className="bg-gradient-to-r from-pink-500 to-violet-600 md:p-5 lg:p-20 xl:p-0 2xl:p-20">
         <div className="p-4 flex flex-col xl:flex-row xl:p-40 xl:justify-center xl:items-center text-white">
-            <div className="xl:w-96 xl:mx-10">
-              <span className="text-title font-semibold">
-                Aprenda qualquer linguagem de programação
-              </span>
-              <p className="text-gray-200 text-sm mt-2 leading-relaxed">
-                Na Thera, todos tiveram o começo com alguma dessas linguagens.
-                Então por que você não teria? Nós iremos lhe passar o caminho a
-                ser seguido e no final haverá tarefas para concluir.
-              </p>
-            </div>
-            <LanguagesGrid />
+          <div className="xl:w-96 xl:mx-10">
+            <span className="text-title font-semibold">
+              Aprenda qualquer linguagem de programação
+            </span>
+            <p className="text-gray-200 text-sm mt-2 leading-relaxed">
+              Na Thera, todos tiveram o começo com alguma dessas linguagens.
+              Então por que você não teria? Nós iremos lhe passar o caminho a
+              ser seguido e no final haverá tarefas para concluir.
+            </p>
+          </div>
+          <LanguagesGrid />
         </div>
       </div>
     </div>
--- my-app/src/app/components/home/ToolSection.js
+++ my-app/src/app/components/home/ToolSection.js
@@ -1,17 +1,27 @@
 import ToolGrid from "../ToolGrid/ToolGrid";
+import DivToolCards from "../ToolGrid/DivToolCards";
 
 export default function ToolSection() {
   return (
     <div>
-      <div className="p-4">
-        <span className="text-title font-semibold">
-          E as melhores ferramentas também
-        </span>
-        <p className="font-light text-sm mt-3 leading-relaxed">
-          Para um dominio eficiente é necessário ter dominio de algumas
-          tecnologias.
-        </p>
-        <ToolGrid />
+      <div className="p-4 bg-gradient-to-t from-slate-50 to-zinc-100 md:p-8 lg:p-24 xl:flex xl:justify-center xl:p-40">
+        <div className="xl:flex-col xl:w-96 xl:justify-center">
+          <span className="text-title font-semibold">
+            E as melhores ferramentas também
+          </span>
+          <p className="font-light text-sm mt-3 leading-relaxed">
+            Para um dominio eficiente é necessário ter dominio de algumas
+            tecnologias.
+          </p>
+        </div>
+        <div className="lg:mt-5 xl:mt-0">
+          <div className="block lg:hidden">
+            <ToolGrid />
+          </div>
+          <div className="hidden lg:block ml-10">
+            <DivToolCards />
+          </div>
+        </div>
       </div>
     </div>
   );
